456713873f8b528d6d548cb5eb93b90e4926ef35
Renan Teixeira <git@renanteixeira.com.br>
2022-05-10 20:08:47 -0300

    [FIX] purchase_request - PR Line - Product name

--- purchase_request/models/purchase_request_line.py
+++ purchase_request/models/purchase_request_line.py
@@ -286,10 +286,10 @@ class PurchaseRequestLine(models.Model):
     def onchange_product_id(self):
         if self.product_id:
             name = self.product_id.name
-            if self.product_id.code:
-                name = "[{}] {}".format(name, self.product_id.code)
+            if self.product_id.default_code:
+                name = "[{}] {}".format(self.product_id.default_code, name)
             if self.product_id.description_purchase:
-                name += "\n" + self.product_id.description_purchase
+                name += " - " + self.product_id.description_purchase
             self.product_uom_id = self.product_id.uom_id.id
             self.product_qty = 1
             self.name = name
