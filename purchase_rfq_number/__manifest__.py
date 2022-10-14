7fde6fb46bf20360bb6030f30cadd22a19839b78
Renan Teixeira <git@renanteixeira.com.br>
2022-10-14 14:38:40 -0300

    [UPD] purchase_rfq_number

--- purchase_rfq_number/__manifest__.py
+++ purchase_rfq_number/__manifest__.py
@@ -4,8 +4,8 @@
 {
     "name": "Purchase For Quotation Numeration",
     "summary": "Different sequence for purchase for quotations",
-    "version": "14.0.1.0.0",
-    "author": "IT Brasil",
+    "version": "14.0.1.1.0",
+    "author": "ProThai, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Purchase Management",
     "depends": ["purchase"],
